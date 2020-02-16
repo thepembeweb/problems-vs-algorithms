bd440c4cd31dd57cb4ca24cb9f4d6fccaa9d6242
Pemberai Sweto <pembeweb@yahoo.com>
2020-02-16 21:39:58 +0200

    fix: update solution for Problem 6

--- problem_6.py
+++ problem_6.py
@@ -1,45 +1,46 @@
-import random
+## Max and Min in a Unsorted Array
 
+# In this problem, we will look for smallest and largest integer from a list of unsorted integers.
+# The code should run in O(n) time. Do not use Python's inbuilt functions to find min and max.
 
 def get_min_max(ints):
     """
     Return a tuple(min, max) out of list of unsorted integers.
-
     Args:
        ints(list): list of integers containing one or more integers
     """
-    if len(ints) == 0:
-        return "Invalid input! Please input a list containing one or more integers."
-    min_integer = 0
-    max_integer = 0
-    for integer in ints:
-        if integer >= 0:
-            min_integer = min(min_integer, integer)
-            max_integer = max(max_integer, integer)
-        else:
-            return "Invalid input"
-    return (min_integer, max_integer)
 
+    #return None if there is less or equal to one element in the given array
+    if len(ints) <= 1:
+        return None
 
-# Example Test Case of Ten Integers
+    #initialize the minimun and maximum as the first element of the array
+    smallest_num = ints[0]
+    largest_num = ints[0]
 
+    for num in ints:
+        #find the smallest element 
+        if num < smallest_num:
+            smallest_num = num
+
+        #find the largest element
+        if num > largest_num:
+            largest_num = num
+
+    return smallest_num, largest_num
+    
+
+## Example Test Case of Ten Integers
+import random
+
+#test 1
 l = [i for i in range(0, 10)]  # a list containing 0 - 9
 random.shuffle(l)
-print(l)
-# should return a tuple (0, 9)
-print("Pass" if ((0, 9) == get_min_max(l)) else "Fail")
 
-ints = [0, -1]
-print(ints)
-# should return a warning message
-print("Pass" if ("Invalid input" == get_min_max(ints)) else "Fail")
+print ("Pass" if ((0, 9) == get_min_max(l)) else "Fail")  #should print pass
 
-ints = [0]
-print(ints)
-# should return (0, 0)
-print("Pass" if ((0, 0) == get_min_max(ints)) else "Fail")
+#test 2
+print("Pass" if ((None) == get_min_max([])) else "False") #should print pass
 
-ints = []
-print(ints)
-# should return a warning message.
-print(get_min_max(ints))
+#test 3
+print("Pass" if ((None) == get_min_max([1])) else "False") #should print pass
