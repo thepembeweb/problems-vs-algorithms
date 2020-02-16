ce98ce3eeaea006ea11b09db39f93fea4aab15e6
Pemberai Sweto <pembeweb@yahoo.com>
2020-02-16 21:21:09 +0200

    fix: update solution for Problem 2

--- problem_2.py
+++ problem_2.py
@@ -1,64 +1,89 @@
 def rotated_array_search(input_list, number):
     """
     Find the index by searching in a rotated sorted array
-
     Args:
        input_list(array), number(int): Input array to search and the target
     Returns:
        int: Index or -1
     """
+
+    #Return -1 if the case input list is empty
     if len(input_list) == 0:
-        print("Please input a valid array.")
         return -1
-    min_number_index = find_min_number_index(input_list)
-    if number > input_list[0]:
-        return binary_search(input_list, number, 0, min_number_index - 1)
-    elif number < input_list[0]:
-        return binary_search(input_list, number, min_number_index, len(input_list) - 1)
+
+    if len(input_list) == 1 and input_list[0] != number:
+        return -1
+
+
+    #initial the left index and right index
+    left_index = 0
+    right_index = len(input_list) - 1
+    
+    
+    while left_index + 1 < right_index:
+        mid_index = (left_index + right_index) //2
+        mid_element = input_list[mid_index]
+
+        
+
+        # return the middle index if the number is equal to the middle element
+        if mid_element == number:
+            return mid_index
+
+        
+
+        # Condition 1: left_element < mid_element < right_element
+
+        if input_list[left_index] < mid_element < input_list[right_index]:
+
+            if number > mid_element:
+
+                left_index = mid_index
+
+            else:
+
+                right_index = mid_index
+
+        # Condition 2: middle_element > left_element and middle_element > right_element
+
+        elif mid_element > input_list[left_index] and mid_element > input_list[right_index]:
+
+            if input_list[left_index] <= number < mid_element:
+
+                right_index = mid_index
+
+            else:
+
+                left_index = mid_index
+
+        # Condition 3: middle_element < left_element and middle_element < right element
+
+        elif mid_element < input_list[left_index] and mid_element < input_list[right_index]:
+
+            if mid_element < number <= input_list[right_index]:
+
+                left_index = mid_index
+
+            else:
+
+                right_index = mid_index
+        
+
+    if input_list[left_index] == number:
+
+        return left_index
+       
+    elif input_list[right_index] == number:
+
+        return right_index
     else:
-        return 0
-
-
-def binary_search(input_list, number, left, right):
-    low = left
-    hight = right
-    while low <= hight:
-        mid = low + ((hight - low) >> 1)
-        if input_list[mid] == number:
-            return mid
-        elif input_list[mid] < number:
-            low = mid + 1
-        else:
-            hight = mid - 1
-    return -1
-
-
-def find_min_number_index(input_list):
-    low = 0
-    hight = len(input_list) - 1
-    if input_list[low] < input_list[hight]:
-        return 0
-
-    while low <= hight:
-        mid = low + ((hight - low) >> 1)
-        if input_list[low] < input_list[mid]:
-            if input_list[mid + 1] < input_list[mid]:
-                return mid + 1
-            low = mid + 1
-        elif input_list[low] > input_list[mid]:
-            if input_list[mid - 1] > input_list[mid]:
-                return mid
-            hight = mid - 1
-        else:
-            return mid
-
+        return -1
 
 def linear_search(input_list, number):
     for index, element in enumerate(input_list):
         if element == number:
-            return index
+             return index
     return -1
-
 
 def test_function(test_case):
     input_list = test_case[0]
@@ -68,7 +93,6 @@ def test_function(test_case):
     else:
         print("Fail")
 
-
 test_function([[6, 7, 8, 9, 10, 1, 2, 3, 4], 6])
 test_function([[6, 7, 8, 9, 10, 1, 2, 3, 4], 1])
 test_function([[6, 7, 8, 1, 2, 3, 4], 8])
@@ -77,3 +101,4 @@ test_function([[6, 7, 8, 1, 2, 3, 4], 10])
 test_function([[1, 2, 3, 4, 6, 7, 8], 1])
 test_function([[1], 0])
 test_function([[], 1])
+test_function([[6, 7, 8, 9, 10, 11, 12, 1, 2, 3, 4], 1])
